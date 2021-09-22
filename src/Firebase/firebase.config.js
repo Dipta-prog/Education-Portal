cdcaaef81193de885b9f6d37f313b4d5a85151c4
Ubaidur Rahman <ubaidurrahmanmcc@gmail.com>
2021-09-23 02:56:26 +0600

    deploy to firebase

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0pDj9pz1Grqcvn_upWUZQWQssxGqvvKA",
+  authDomain: "education-portal-4a4df.firebaseapp.com",
+  projectId: "education-portal-4a4df",
+  storageBucket: "education-portal-4a4df.appspot.com",
+  messagingSenderId: "852446107859",
+  appId: "1:852446107859:web:d6e155a2c2705781175c26"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
