614584bda2b976c127669d15530fd5d548bcf450
Ubaidur Rahman <ubaidurrahmanmcc@gmail.com>
2021-09-23 06:51:08 +0600

    Text content changed

--- src/Component/Home/Home/EveryStepSection/EveryStepSection.js
+++ src/Component/Home/Home/EveryStepSection/EveryStepSection.js
@@ -1,5 +1,4 @@
 import React from "react";
-import everyStepSectionImg from "../../../../Media/img/Home-Img/every-step-section-img/every-step-section.jpg";
 import "./EveryStepSection.scss";
 const EveryStepSection = () => {
   return (
@@ -10,7 +9,7 @@ const EveryStepSection = () => {
           We Are With You Every Step <br />
           Get Started Easily.
         </h3>
-        <h6>Make your Website Faster, Safer & Better Support.</h6>
+        <h6>Lorem ipsum dolor sit amet consectetur.</h6>
         <button className="btn btn-success">GET STARTED NOW !</button>
       </div>
     </div>
