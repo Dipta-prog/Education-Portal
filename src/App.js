a3404fc2c38787941c4c7766fdd5c4d487f8309f
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-09-26 09:33:17 +0600

    text decoration removed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import "./App.scss";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.scss";
 import Home from "./Component/Home/Home/Home";
 import Login from "./Firebase/Login/Login/Login";
 function App() {
--- src/Component/Shared/Navbar/Navbar.js
+++ src/Component/Shared/Navbar/Navbar.js
@@ -5,19 +5,19 @@ import './Navbar.scss';
 const Navbar = () => {
     return (
         <nav className="nav-area">
-        <input type="checkbox" id="box" />
-        <label for="box" className="btn-area"><i className="fa fa-align-left"></i></label>
-        <label>
-            <img src={logo} alt="" className="logo" />
-        </label>
-        <ul className="nav-links">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Events</a></li>
-        </ul>
-    </nav>
+            <input type="checkbox" id="box" />
+            <label for="box" className="btn-area"><i className="fa fa-align-left"></i></label>
+            <label>
+                <img src={logo} alt="" className="logo" />
+            </label>
+            <ul className="nav-links">
+                <li><a className='nav-deco' href="#">Home</a></li>
+                <li><a className='nav-deco' href="#">About</a></li>
+                <li><a className='nav-deco' href="#">Contact</a></li>
+                <li><a className='nav-deco' href="#">Services</a></li>
+                <li><a className='nav-deco' href="#">Events</a></li>
+            </ul>
+        </nav>
     );
 };
 
