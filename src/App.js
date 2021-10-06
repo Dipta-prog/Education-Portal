2cb195ea6f037536d2a9d5f92eb6a9bf8549b4c7
raju10 <trolltube272@gmail.com>
2021-10-07 03:18:29 +0600

    login part design

--- src/App.js
+++ src/App.js
@@ -1,37 +1,60 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import CourseDetails from '../src/Component/Pages/CourseDetails/CourseDetails';
+import CourseDetails from "../src/Component/Pages/CourseDetails/CourseDetails";
 import "./App.scss";
 import Home from "./Component/Home/Home/Home";
+import Deshbord from "./Component/Pages/Deshbord/Deshbord";
+import StudentRegisterForm from "./Component/Pages/Deshbord/StudentRegisterForm/StudentRegisterForm";
+import StudentComment from "./Component/Pages/StudentComment/StudentComment";
 import TeacherForm from "./Component/Pages/TeacherForm/TeacherForm";
 import SIdebar from "./Component/Shared/Sidebar/SIdebar";
 import Login from "./Firebase/Login/Login/Login";
-
+import NewLogin from "./Firebase/NewLogin/NewLogin";
+export const UseSelcCourseContext = createContext();
 function App() {
+  const [studentSelactedCourse, setStudentSelactedCourse] = useState({});
   return (
     <div>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route path="/home">
-            <Home></Home>
-          </Route>
-          <Route path="/login">
-            <Login></Login>
-          </Route>
-          <Route path="/courseDetails">
-            <CourseDetails />
-          </Route>
-          <Route path="/teacherForm">
-            <TeacherForm />
-          </Route>
-          <Route path="/sidebar">
-            <SIdebar />
-          </Route>
-        </Switch>
-      </Router>
+      <UseSelcCourseContext.Provider
+        value={[studentSelactedCourse, setStudentSelactedCourse]}
+      >
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path="/home">
+              <Home></Home>
+            </Route>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+            <Route path="/courseDetails">
+              <CourseDetails />
+            </Route>
+            <Route path="/teacherForm">
+              <TeacherForm />
+            </Route>
+            <Route path="/sidebar">
+              <SIdebar />
+            </Route>
+            {/* arif */}
+            <Route path="/deshbord">
+              <Deshbord></Deshbord>
+            </Route>
+            <Route path="/student-register">
+              <StudentRegisterForm></StudentRegisterForm>
+            </Route>
+            <Route path="/student-comment">
+              <StudentComment></StudentComment>
+            </Route>
+            <Route path="/newLogin">
+              <NewLogin></NewLogin>
+            </Route>
+            {/*  */}
+          </Switch>
+        </Router>
+      </UseSelcCourseContext.Provider>
     </div>
   );
 }
